c59520c49cee9a7b5e7315bcd49880cdc0d19191
Dzonny_cp <SND\Dzonny_cp>
2015-07-24 06:17:03 -0700

    Project restructuralization, part I (split to parts because of SVN issues)

---
+++ XmlKeyboardEdit/GlobalAssemblyInfo.cpp
@@ -0,0 +1,9 @@
+﻿using namespace System::Reflection;
+using namespace System::Resources;
+
+[assembly:AssemblyCompany("Đonny")];
+[assembly:AssemblyProduct("XmlKeyboardEdit")];
+[assembly:AssemblyCopyright("Copyright © Đonny 2013-2015")];
+[assembly:AssemblyVersion("0.2.0.*")];
+[assembly:AssemblyFileVersion("0.2.0.0")];
+[assembly:NeutralResourcesLanguage("en")];
---
+++ XmlKeyboardEdit/GlobalAssemblyInfo.vb
@@ -0,0 +1,9 @@
+﻿Imports System.Reflection
+Imports System.Resources
+
+<Assembly: AssemblyCompany("Đonny")>
+<Assembly: AssemblyProduct("XmlKeyboardEdit")>
+<Assembly: AssemblyCopyright("Copyright © Đonny 2013-2015")>
+<Assembly: AssemblyVersion("0.2.0.*")>
+<Assembly: AssemblyFileVersion("0.2.0.0")>
+<Assembly: NeutralResourcesLanguage("en")>
